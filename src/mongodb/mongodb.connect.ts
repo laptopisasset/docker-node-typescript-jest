e219bfb21bb836fcc2d60338498accf4eb37b375
Soundhar <laptopisasset@gmail.com>
2021-05-01 23:39:52 +0530

    Added warning offs.

--- src/mongodb/mongodb.connect.ts
+++ src/mongodb/mongodb.connect.ts
@@ -3,7 +3,8 @@ import { connect } from "mongoose";
 export const DBINIT = async () => {
   try {
     await connect(
-      `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DATABASE}`
+      `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DATABASE}`,
+      { useNewUrlParser: true, useUnifiedTopology: true }
     );
   } catch (err) {
     console.log("Error connecting to mongodb");
