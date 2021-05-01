d2a92ac24d1915414cf8d1d001c8f4e848c895e8
Soundhar <laptopisasset@gmail.com>
2021-05-01 22:40:51 +0530

    removed hello world.

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,6 @@ import express from "express";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.json("hello world!");
-});
-
 app.listen(process.env.PORT, () => {
   console.log(`Server is running in ${process.env.PORT}`);
 });
