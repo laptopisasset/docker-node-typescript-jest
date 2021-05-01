5388c36843dd65ebbef03dc920ca2fe6988941a4
Soundhar <laptopisasset@gmail.com>
2021-05-01 22:58:44 +0530

    Added server.

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,4 @@ import express from "express";
 
 const app = express();
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running in ${process.env.PORT}`);
-});
-
 export default app;
