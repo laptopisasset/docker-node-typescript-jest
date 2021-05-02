a54f71207026c19bce4946acca0ff8ad2ea9a2bb
Soundhar <laptopisasset@gmail.com>
2021-05-02 08:45:58 +0530

    Get todos int completed.

--- src/routes/todos.routes.ts
+++ src/routes/todos.routes.ts
@@ -4,5 +4,6 @@ import TodoController from "../controllers/todo.controller";
 const router = Router();
 
 router.post("/", TodoController.createTodo);
+router.get("/", TodoController.getTodos);
 
 export default router;
--- tests/integration/todo.controller.int.test.ts
+++ tests/integration/todo.controller.int.test.ts
@@ -6,6 +6,15 @@ import newTodo from "../mocks/new-todo.json";
 const endpointUrl = "/todos/";
 
 describe(endpointUrl, () => {
+  it(["GET", endpointUrl].join(" "), async () => {
+    const response = await request(app).get(endpointUrl);
+
+    expect(response.status).toBe(200);
+    // expect(typeof response.body).toBe("array");
+    expect(Array.isArray(response.body)).toBeTruthy();
+    expect(response.body[0].title).toBeDefined();
+    expect(response.body[0].done).toBeDefined();
+  });
   it(["POST", endpointUrl].join(" "), async () => {
     const response = await request(app).post(endpointUrl).send(newTodo);
     expect(response.status).toBe(201);
