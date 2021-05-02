fb094da32fc463cb877371f29dff95759f5bcd7d
Soundhar <laptopisasset@gmail.com>
2021-05-02 10:14:37 +0530

    Added int test for getTodoById.

--- src/routes/todos.routes.ts
+++ src/routes/todos.routes.ts
@@ -5,5 +5,6 @@ const router = Router();
 
 router.post("/", TodoController.createTodo);
 router.get("/", TodoController.getTodos);
+router.get("/:todoId", TodoController.getTodoById);
 
 export default router;
--- tests/integration/todo.controller.int.test.ts
+++ tests/integration/todo.controller.int.test.ts
@@ -4,7 +4,7 @@ import app from "../../src/app";
 import newTodo from "../mocks/new-todo.json";
 
 const endpointUrl = "/todos/";
-
+let firstTodo: { _id: string; title: string; done: boolean };
 describe(endpointUrl, () => {
   it(["GET", endpointUrl].join(" "), async () => {
     const response = await request(app).get(endpointUrl);
@@ -14,7 +14,29 @@ describe(endpointUrl, () => {
     expect(Array.isArray(response.body)).toBeTruthy();
     expect(response.body[0].title).toBeDefined();
     expect(response.body[0].done).toBeDefined();
+    firstTodo = response.body[0];
   });
+  it(["GET by Id", [endpointUrl, ":todoId"].join()].join(" "), async () => {
+    const response = await request(app).get(
+      [endpointUrl, firstTodo._id].join("")
+    );
+    expect(response.status).toBe(200);
+    expect(response.body.title).toBe(firstTodo.title);
+    expect(response.body.done).toBe(firstTodo.done);
+  });
+
+  it(
+    ["GET todo by id doesn't exsist", [endpointUrl, ":todoId"].join()].join(
+      " "
+    ),
+    async () => {
+      const response = await request(app).get(
+        [endpointUrl, "608e2d9819caaf004d36ce90"].join("")
+      );
+      expect(response.status).toBe(404);
+    }
+  );
+
   it(["POST", endpointUrl].join(" "), async () => {
     const response = await request(app).post(endpointUrl).send(newTodo);
     expect(response.status).toBe(201);
